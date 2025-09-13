89973a838071ddf4b940f40f4544279905d45371
Gabriel Pivoto <gabrielspivoto@gmail.com>
2025-09-13 20:09:12 -0300

    feat: add orders page client

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,6 @@
+// server component (sem "use client")
+import { OrdersPageClient } from '@/features/orders/components/OrdersPageClient';
+
+export default function Page() {
+  return <OrdersPageClient />;
+}
